080ddfce55d4689bf50d0f900a2a6d778b178204
VitorLamego <vitorlamegoo@gmail.com>
2023-03-28 13:26:39 -0300

    Adding controller to hour input fields

--- src/routes/config/components/ActivityRow.svelte
+++ src/routes/config/components/ActivityRow.svelte
@@ -1,15 +1,81 @@
-<script>
+<script lang="ts">
 	import { startHour, finishHour } from '$lib/store';
+	import { imask } from '@imask/svelte';
+
+	const hourMask = {
+		mask: '[#]0:&0',
+		definitions: {
+			'#': /[0-2]/,
+			'&': /[0-5]/
+		}
+	};
+
+	function updateHour(isStartHour: boolean) {
+		const startHourInput = document.getElementById('startHour') as HTMLInputElement;
+		const finishHourInput = document.getElementById('finishHour') as HTMLInputElement;
+
+		const startContent = startHourInput?.value;
+		const finishContent = finishHourInput?.value;
+
+		const startSplitTime = startContent.split(':');
+		const finishSplitTime = finishContent.split(':');
+
+		const preStartHourValue = $startHour;
+		const preFinishHourValue = $finishHour;
+
+		if (startContent != undefined && finishContent != undefined) {
+			const startHourInSeconds = parseInt(startSplitTime[0]) * 3600;
+			const startMinutesInSeconds = parseInt(startSplitTime[1]) * 60;
+			const startFinalTime = startHourInSeconds + startMinutesInSeconds;
+
+			const finishHourInSeconds = parseInt(finishSplitTime[0]) * 3600;
+			const finishMinutesInSeconds = parseInt(finishSplitTime[1]) * 60;
+			const finishFinalTime = finishHourInSeconds + finishMinutesInSeconds;
+
+			if (isStartHour) {
+				if (startFinalTime > finishFinalTime) {
+					startHourInput.value = preStartHourValue;
+					console.log('Star Hour: Alteração não realizada');
+					return;
+				}
+				console.log('Star Hour: Alteração realizada');
+
+				startHour.set(startContent);
+			} else {
+				if (finishFinalTime < startFinalTime) {
+					finishHourInput.value = preFinishHourValue;
+					return;
+				}
+				finishHour.set(finishContent);
+			}
+		}
+	}
 </script>
 
 <ion-grid>
 	<ion-row class="activity-row">
 		<div class="hour-input">
-			<ion-input class="custom" type="text" inputmode="numeric" value={$startHour} />
+			<ion-input
+				class="custom"
+				type="text"
+				inputmode="numeric"
+				value={$startHour}
+				id="startHour"
+				use:imask={hourMask}
+				on:ionBlur={() => updateHour(true)}
+			/>
 		</div>
 		<div class="text">a</div>
 		<div class="hour-input">
-			<ion-input class="custom" type="text" inputmode="numeric" value={$finishHour} />
+			<ion-input
+				class="custom"
+				type="text"
+				inputmode="numeric"
+				value={$finishHour}
+				id="finishHour"
+				use:imask={hourMask}
+				on:ionBlur={() => updateHour(false)}
+			/>
 		</div>
 	</ion-row>
 </ion-grid>
